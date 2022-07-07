79aeba8a17f3167c41878c5c5e37418b3e66061b
marioherlein@hotmail.com <marioherlein@hotmail.com>
2022-07-07 15:15:59 -0300

    Se generó la estructura del dataframe en el extract

--- ETL/extract.py
+++ ETL/extract.py
@@ -1,3 +1,4 @@
+import logging
 import pandas as pd
 import requests
 import json
@@ -12,7 +13,7 @@ with open('./config.json', 'r') as file:
 
 class Connection():
     """
-    Esta Clase permite conectarse a la API  que se conecta a la BDD del GC3
+    This Class allows connecting to the API that connects to the GC3 DB
     """
     USER=None
     PASS=None
@@ -38,23 +39,24 @@ class Connection():
         }
         response= requests.request("POST",self.URLTOKEN,data=self.payload)
         token=response.json()
-        print("Generando token")
         self.token = token["token"]
+        logging.info(f"Generando token: {self.token}")
+        print("Generando token")
         
 
 
 class Search():
-    
-    
+
+
     def vessel_by_Mmsi(mmsi):
-        """Usando la API Arcgis, para buscar el ID de elementos"
+        """Using the Arcgis API, to look up the ID of elements"
 
         Args:
-            mmsi (string): 
+            mmsi (string):MMSI number of the vessel to search
 
         Returns:
-            _type_: _description_
-        """        
+            elementId(string): the unique ID of elements in th db
+        """
         mmsi = str(mmsi)
         payload = json.dumps({"criteria": {"MMSI": mmsi,"elementType": "buque"}})
         headers = {'Content-Type': 'application/json'}
@@ -67,17 +69,18 @@ class Search():
         except:
             print(f"MMSI {mmsi} not found")
             return None
-    
-    def trackSearch(token,mmsi,desde,hasta):
-        """  Usando la conexión a la API, busca las posiciones del buque por MMSI, acorde las fechas determinadas
-        Args:
-            mmsi (str): Numero MMSI del buque a buscar
-            from (datetime): Fecha desde donde comienza la busqueda
-            finish (datetime): Fecha hasta donde finaliza la busqueda
 
-        Returns:
-            json:Contiene todos los datos de posiciones del buque, incluidas las fuentes de datos """
-        
+    def trackSearch(token,mmsi,desde,hasta):
+        """  Using API connection, look up vessel positions by MMSI, according to given dates
+        Args:
+            mmsi (str): MMSI number of the vessel to search
+            from (datetime): Date from where the search starts
+            finish (datetime): Date until where the search ends
+
+        returns:
+            json: Contains all position data of the ship, including data sources
+        """
+
         url=f"https://sig.prefecturanaval.gob.ar/apiadmin/track/get?f=json&token={token}"
         ElementId=Search.vessel_by_Mmsi(mmsi)
         payload = json.dumps({"ElementId": ElementId,
@@ -88,21 +91,22 @@ class Search():
         "addStatistics": True
         })
         headers = {'Content-Type': 'application/json'}
-        
-        response = requests.request("POST", url, headers=headers, data=payload)
         print("Buscando Posiciones")
-        
+        response = requests.request("POST", url, headers=headers, data=payload)
         return response.json()
 
 
     def trackDataframe(json):
-        """Dataframe generator
-
-        Args:
-            json (json): Json from Search.trackSearch with tackVessel information
+        """
+        It takes a json file, loops through it, and creates a dataframe with the data
+        
+        :param json: the json file
+        :return: A dataframe with the following columns:
+        FH, SOG, COG, X, Y
         """
         positions=json["positions"]
         list_positions=[]
+        list_KeyError=[]
         position={}
 
         for pos in tqdm(positions):
@@ -112,12 +116,16 @@ class Search():
             try:
                         position.update({"X":pos["location"]["geo"]["coordinates"][0],
                  "Y":pos["location"]["geo"]["coordinates"][1]})
-            
+
             except KeyError:
-                print( f" El ObjectId {pos['objectId']}, no tiene coordenadas")
+                # print( f" El ObjectId {pos['objectId']}, no tiene coordenadas")
+                list_KeyError.append(pos['objectId'])
                 continue
+
             list_positions.append(position)
         df=pd.DataFrame(list_positions)
+
+        print("Porcentaje de ObjectId sin coordenadas: ", round((len(list_KeyError)*100)/len(df),2),"%" )
 
         return df
 
--- main.py
+++ main.py
@@ -1,57 +1,44 @@
+import logging
 import json
 from ETL.Extract import Connection , Search
 
+def main():
+    """
+    It reads a json file, then it creates a connection object, then it gets a token, then it gets a
+    start and finish date, then it gets a mmsi.
+    """
+    logging.basicConfig(filename="logsdesa/nereo.log", format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S', level=logging.INFO)
+    logging.info("Iniciando Nereo")
 
-with open("./config.json", "r") as f:
-    config= json.load(f)
+    with open("./config.json", "r") as f:
+        config= json.load(f)
 
-ENTORNO="PROD"
-conexion = Connection(USER = config[ENTORNO]['user']
-                    , PASS = config[ENTORNO]['pass']
-                    , URLTOKEN = config[ENTORNO]['urlToken']
-                    , REFERER = config[ENTORNO]['referer'] )
-
-
-
-token=conexion.token
-start='2022-05-02'
-finish='2022-07-04'
-mmsi="701000516"
-
-
-
-busqueda=Search.trackSearch(token,mmsi,start,finish) 
-
-
-df=Search.trackDataframe(busqueda)
-
-
-print(df)
-
-print(busqueda["positions"][-1])
-
-# posiciones=busqueda["positions"]
+    ENTORNO="PROD"
+    conexion = Connection(USER = config[ENTORNO]['user']
+                        , PASS = config[ENTORNO]['pass']
+                        , URLTOKEN = config[ENTORNO]['urlToken']
+                        , REFERER = config[ENTORNO]['referer'] )
 
 
 
-# # print(busqueda["positions"])
+    token=conexion.token
+    start='2022-07-02'
+    finish='2022-07-04'
+    mmsi="701006867"
 
-# print(len(posiciones))
-# for index, a in enumerate(posiciones):
-#     print("número de orden", index)
-#     print("COG =", a['CourseOverGround'])
-#     print("Time =", a["msgTime"])
-#     print("Coordenadas =")
-#     print("_____________Latitud: ",a["location"]["geo"]["coordinates"][1] )
-#     print("_____________Longitud:",a["location"]["geo"]["coordinates"][0] )
 
-# print(len(busqueda["positions"]))
 
-# print("Busqueda")
-# # a=busqueda["features"]
-# # c=a[1]["attributes"]
-# print(type(busqueda))
-# # print(len(a))
-# print("Finalizado")
+    busqueda=Search.trackSearch(token,mmsi,start,finish)
 
-# # print(conexion.token)
+
+    df=Search.trackDataframe(busqueda)
+
+
+    print(df.SOG.max())
+
+    print(busqueda["positions"][-1])
+
+
+
+if __name__ == '__main__':
+    main()
